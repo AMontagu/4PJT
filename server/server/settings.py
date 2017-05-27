e2712e71d37aad2aec75d967d5302b90f404eeec
Unknown <adrienmontagu@gmail.com>
2017-05-27 22:28:28 +0200

    test SQL Database

--- server/server/settings.py
+++ server/server/settings.py
@@ -14,9 +14,9 @@ import os
 import sys
 
 if sys.platform.startswith('win'):
-    LINUX = False
+	LINUX = False
 else:
-    LINUX = True
+	LINUX = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,82 +31,84 @@ SECRET_KEY = '0+8wbfxid2pyk%iog2^jy$q^oa!-uceymd3mcz2#0an+efes%i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+DEBUGDATABASE = False
+
 ALLOWED_HOSTS = ['localhost', 'localhost:8000', '127.0.0.1', '*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'corsheaders',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'channels',
-    'project',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'corsheaders',
+	'rest_framework',
+	'rest_framework.authtoken',
+	'channels',
+	'project',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'corsheaders.middleware.CorsMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-       'rest_framework.permissions.AllowAny',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'project.authentication.ExpiringTokenAuthentication',
-        #'rest_framework.authentication.TokenAuthentication',
-    )
+	'DEFAULT_PERMISSION_CLASSES': (
+	   'rest_framework.permissions.AllowAny',
+	),
+	'DEFAULT_AUTHENTICATION_CLASSES': (
+		'project.authentication.ExpiringTokenAuthentication',
+		#'rest_framework.authentication.TokenAuthentication',
+	)
 }
 
 ROOT_URLCONF = 'server.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-	    ,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [os.path.join(BASE_DIR, 'templates')]
+		,
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 if LINUX:
-    CHANNEL_LAYERS = {
-        "default": {
-            "BACKEND": "asgi_redis.RedisChannelLayer",
-            "CONFIG": {
-                "hosts": [("localhost", 6379)],
-            },
-            "ROUTING": "server.routing.channel_routing",
-        },
-    }
+	CHANNEL_LAYERS = {
+		"default": {
+			"BACKEND": "asgi_redis.RedisChannelLayer",
+			"CONFIG": {
+				"hosts": [("localhost", 6379)],
+			},
+			"ROUTING": "server.routing.channel_routing",
+		},
+	}
 else:
-    CHANNEL_LAYERS = {
-        "default": {
-            "BACKEND": "asgiref.inmemory.ChannelLayer",
-            "ROUTING": "server.routing.channel_routing",
-        },
-    }
+	CHANNEL_LAYERS = {
+		"default": {
+			"BACKEND": "asgiref.inmemory.ChannelLayer",
+			"ROUTING": "server.routing.channel_routing",
+		},
+	}
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -115,31 +117,41 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+if DEBUGDATABASE:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		}
+	}
+else:
+	DATABASES = {
+		'default': {
+			'NAME': 'master',
+			'ENGINE': 'sqlserver_ado',
+			'HOST': '192.168.30.51',
+			'USER': 'Qwirk.lan\\Administrator',
+			'PASSWORD': 'Supinf0',
+		}
+	}
 
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
 
 
